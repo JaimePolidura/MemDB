a6519b596b9b79d717301d2d4a437c8c778b61ae
JaimeTruman <jaime.polidura@gmail.com>
2022-12-08 11:04:39 +0100

    tcpserver accepts requesat

--- src/connection/TCPServer.h
+++ src/connection/TCPServer.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <boost/asio.hpp>
 #include "../Users/Authenticator.h"
+#include "../Messages/MessageParser.h"
 
 using namespace boost::asio;
 
@@ -12,29 +13,37 @@ private:
     Authenticator authenicator;
     io_context ioContext;
     ip::tcp::acceptor acceptator;
-
-    static const char flag1Mask = 0x80; //1000 0000
-    static const char flag2Mask = 0x40; //0100 0000
+    MessageParser messageParser;
 
 public:
     TCPServer(short unsigned port, Authenticator authenicator): port(port), authenicator(std::move(authenicator)),
                                                                 acceptator(ioContext, ip::tcp::endpoint{ip::tcp::v4(), this->port}) {};
 
     void run() {
-        while(true) {
+        printf("[Server] Waiting for conenctions...\n");
+
+        try{
             this->waitForConnectoins();
+
+            this->ioContext.run();
+        }catch (std::exception& e) {
+            printf("[Server] fatal error\n");
         }
     }
 
 private:
     void waitForConnectoins() {
-        this->acceptator.async_accept([this](std::error_code ec, boost::asio::ip::tcp::socket socket) {
-            this->handleNewConnection(socket);
+        this->acceptator.async_accept([this](std::error_code ec, ip::tcp::socket socket) {
+            printf("[Server] Accepted connection\n");
+
+            size_t bytes = socket.available();
+            std::vector<uint8_t> buffer(bytes);
+
+            socket.async_read_some(boost::asio::buffer(buffer), [this](const boost::system::error_code& error, std::size_t bytesRead) {
+                printf("[Server] Read %llu bytes\n", bytesRead);
+            });
+
             this->waitForConnectoins();
         });
-    }
-
-    void handleNewConnection(boost::asio::ip::tcp::socket& socket) {
-        //TODO Proceed
     }
 };

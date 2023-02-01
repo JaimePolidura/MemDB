ac49a82d0bd44bf2ff67451f2bf3397c82e04a8f
JaimeTruman <jaime.polidura@gmail.com>
2023-02-01 18:47:28 +0100

    fixed bug in read write lock

--- core/src/utils/threads/ReadWriteLock.h
+++ core/src/utils/threads/ReadWriteLock.h
@@ -5,45 +5,34 @@
 
 class ReadWriteLock {
 private:
-    std::atomic_int64_t readersCount; //Readers in crital section
-    std::atomic_int64_t writersCount; //Writer in critial section + nº writers waiting
-    std::mutex writerLock;
-
-    std::condition_variable noWriterCondition;
-    std::mutex noWriterConditionLock;
-
-    std::condition_variable noReadersCondition;
-    std::mutex noReadersConditionLock;
+    int64_t readersCount;
+    std::mutex readCounterLock;
+    std::mutex lock;
 
 public:
     void lockRead() {
-        std::unique_lock noWriterConditionUniqueLock(this->noWriterConditionLock);
-        this->noWriterCondition.wait(noWriterConditionUniqueLock, [this]{return this->writersCount.load() == 0;});
+        this->readCounterLock.lock();
 
-        this->readersCount.fetch_add(1);
+        if(++this->readersCount == 1)
+            this->lock.lock();
+
+        this->readCounterLock.unlock();
     }
 
     void unlockRead() {
-        this->readersCount.fetch_sub(1);
+        this->readCounterLock.lock();
 
-        if(this->readersCount.load() == 0)
-            this->noReadersCondition.notify_one();
+        if(--this->readersCount == 0)
+            this->lock.unlock();
+
+        this->readCounterLock.unlock();
     }
 
     void lockWrite() {
-        std::unique_lock noReadersConditionUniqueLock(this->noReadersConditionLock);
-
-        this->writersCount.fetch_add(1);
-        this->noReadersCondition.wait(noReadersConditionUniqueLock, [this]{return this->readersCount.load() == 0;});
-
-        this->writerLock.lock();
+        this->lock.lock();
     }
 
     void unlockWrite() {
-        this->writersCount.fetch_sub(1);
-        if(this->writersCount.load() == 0)
-            this->noWriterCondition.notify_all();
-
-        this->writerLock.unlock();
+        this->lock.unlock();
     }
 };

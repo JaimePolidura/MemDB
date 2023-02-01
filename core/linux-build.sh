e6b2b75b216f0614e7b24a89c6ab80967c8b8f26
JaimeTruman <jaime.polidura@gmail.com>
2023-02-01 19:58:27 +0100

    added bash script to build linux deployment

---
+++ core/linux-build.sh
@@ -0,0 +1,30 @@
+#Delete initial unused files
+rm -r libs/googletest
+rm -r tst
+rm -r playground
+rm -r cmake-build-debug-coverage
+rm -r cmake-build-debug
+rm -r .idea
+
+cd src
+
+#Delete build if exists
+if [ -f ./build ]; then
+ rm -r build
+fi
+
+#Calling cmkae
+cmake -S .. -O ./build -D BUILDING_LINUX_DEPLOYMENT=ON
+
+#Delete main method in CMakeCXXCompilerId.cpp in that way there is no second definitions
+CMAKE_VERSION=$(cmake --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+cd ./build/CMakeFiles/"$CMAKE_VERSION"/CompilerIdCXX
+
+LINE_TO_START_REMOVING=$(grep -n 'main' CMakeCXXCompilerId.cpp | head -n 1 | cut -d : -f 1)
+sed -i "${LINE_TO_START_REMOVING},\$d" CMakeCXXCompilerId.cpp
+
+#Calling make
+cd ../../..
+make
+
+echo "BUILD COMPLETED"

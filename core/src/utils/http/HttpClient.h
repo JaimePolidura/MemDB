ccfbd5151f3675d2daeac14b4d29f8abd1d0ba37
JaimeTruman <jaime.polidura@gmail.com>
2023-02-25 12:29:55 +0100

    changed cpr library to boost

--- core/src/utils/http/HttpClient.h
+++ core/src/utils/http/HttpClient.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
 #include <string>
 #include <map>
-#include <cpr/cpr.h>
+
+using tcp = boost::asio::ip::tcp;
+namespace http = boost::beast::http;
 
 struct HttpResponse {
     nlohmann::json body;
-    int code;
+    uint32_t code;
 
     bool isSuccessful() {
         return code >= 200 && code < 300;
@@ -23,17 +29,37 @@ public:
     static HttpResponse post(const std::string& url,
                              const std::map<std::string, std::string>& body = {},
                              const std::string& authToken = "") {
+        boost::asio::io_context ioc;
+        tcp::resolver resolver(ioc);
+        tcp::socket socket(ioc);
 
-        cpr::Header headers = {{"Content-Type", "application/json"}};
+        auto const results = resolver.resolve(url, "http");
+        boost::asio::connect(socket, results.begin(), results.end());
 
-        if(authToken != "")
-            headers.insert({{"Authorization", "Bearer " + authToken}});
+        http::request<http::string_body> req{http::verb::post, "post", 11};
+        req.set(http::field::host, url);
+        req.set(http::field::user_agent, "Boost");
+        req.set(http::field::content_type, "application/json");
+        if(!authToken.empty()) req.set(http::field::authorization, "Bearer " + authToken);
 
-        cpr::Response response = cpr::Post(cpr::Url(url), body, headers, cpr::Timeout(30 * 1000));
+        nlohmann::json json(body);
+        req.body() = json.dump();
+
+        req.prepare_payload();
+
+        http::write(socket, req);
+
+        boost::beast::flat_buffer buffer;
+        http::response<http::dynamic_body> res;
+        http::read(socket, buffer, res);
+
+        auto responseString = boost::beast::buffers_to_string(res.body().data());
+
+        socket.close();
 
         return HttpResponse{
-                .body = nlohmann::json::parse(response.text),
-                .code = response.status_code
+                .body = nlohmann::json::parse(responseString),
+                .code = res.result_int()
         };
     }
 };

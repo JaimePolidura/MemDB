f1f369e6d222c7fac6438cea9579025ebf75b0eb
Jaime Polidura <jaime.polidura@gmail.com>
2023-05-08 15:25:46 +0200

    modifed getAllNodesController to return the neighbors nodes in the controller

--- clustermanager/src/nodes/get_all_nodes_controller.go
+++ clustermanager/src/nodes/get_all_nodes_controller.go
@@ -1,8 +1,11 @@
 package nodes
 
 import (
+	configuration "clustermanager/src/_shared/config"
+	configuration_keys "clustermanager/src/_shared/config/keys"
 	"clustermanager/src/_shared/logging"
 	"clustermanager/src/nodes/nodes"
+	partitions2 "clustermanager/src/partitions/partitions"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"strconv"
@@ -13,13 +16,23 @@ type GetAllNodesResponse struct {
 }
 
 type GetAllNodeController struct {
-	NodesRepository nodes.NodeRepository
-	Logger          *logging.Logger
+	PartitionRepository partitions2.PartitionRepository
+	Configuartion       configuration.Configuartion
+	NodesRepository     nodes.NodeRepository
+	Logger              *logging.Logger
 }
 
 func (controller *GetAllNodeController) GetAllNodes(context echo.Context) error {
-	nodes, err := controller.NodesRepository.FindAll()
-	
+	var nodes []nodes.Node
+	var err error
+	nodeId := context.QueryParam("nodeId")
+
+	if controller.Configuartion.GetBoolean(configuration_keys.MEMDB_CLUSTERMANAGER_USE_PARTITIONS) {
+		nodes, err = controller.getNodesNeighborsFromPartition(nodeId)
+	} else {
+		nodes, err = controller.NodesRepository.FindAll()
+	}
+
 	if err != nil {
 		controller.Logger.Info("Error while executing GetAllNodesController: " + err.Error())
 		return err
@@ -30,4 +43,23 @@ func (controller *GetAllNodeController) GetAllNodes(context echo.Context) error 
 	return context.JSON(http.StatusOK, GetAllNodesResponse{
 		Nodes: nodes,
 	})
+}
+
+func (controller *GetAllNodeController) getNodesNeighborsFromPartition(nodeIdString string) ([]nodes.Node, error) {
+	nodeId := nodes.NodeId_t(nodeIdString)
+	ringEntries, err := controller.PartitionRepository.GetRingEntriesSorted()
+	nodesPerPartition, err := controller.PartitionRepository.GetNodesPerPartition()
+	if err != nil {
+		return []nodes.Node{}, err
+	}
+
+	var nodes []nodes.Node
+	neighborsEntries, _ := ringEntries.GetNeighborsByNodeId(nodeId, nodesPerPartition)
+
+	for _, entry := range neighborsEntries {
+		node, _ := controller.NodesRepository.FindById(entry.NodeId)
+		nodes = append(nodes, node)
+	}
+
+	return nodes, nil
 }

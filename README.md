fed9e29b86f7f22fc5f66f8d6f1db6932c3b0c2e
Jaime <jaime.polidura@gmail.com>
2023-06-18 17:12:04 +0200

    fixed tst

--- README.md
+++
@@ -1,7 +0,0 @@
-# MemDB
-
-A simple, distributed in-memory key-value database
-
-## Components
-- [Core](/core/README.md)
-- [ClusterManager](/clustermanager/README.md)
--- _deployment/no-cluster/run-simple-cluster.sh
+++
@@ -1,5 +0,0 @@
-docker-compose down -v --remove-orphans
-docker stop $(docker ps -a -q --filter "name=memdb-*")
-docker rm $(docker ps -a -q --filter "name=memdb-*")
-
-docker-compose up --no-deps memdb-instance-1

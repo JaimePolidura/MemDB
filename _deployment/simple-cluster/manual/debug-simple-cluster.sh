2b39251de3caa5585d7bbe1e25887876f4fdb08a
Jaime Polidura <73758994+JaimeTruman@users.noreply.github.com>
2023-09-12 14:25:43 +0200

    Removed boost & removed docker clusterdb in manual simple cluster deployment

--- _deployment/simple-cluster/manual/debug-simple-cluster.sh
+++ _deployment/simple-cluster/manual/debug-simple-cluster.sh
@@ -1,25 +1,16 @@
 cd ../../..
+rm -r /etc/memdb1
+rm -r /etc/memdb2
 
 # CLUSTER DB
-sudo docker stop $(docker ps -a -q --filter "name=memdb-*")
-sudo docker rm $(docker ps -a -q --filter "name=memdb-*")
+etcdctl del --prefix ""
 
-sudo docker run -d \
-  --name memdb-clusterdb \
-  -p 2379:2379 \
-  -e ALLOW_NONE_AUTHENTICATION=yes \
-  -e ETCDCTL_API=3 \
-  --log-driver none \
-  --restart always \
-  bitnami/etcd:latest
-
-sleep 5s
-
-docker exec -it memdb-clusterdb etcdctl put /nodes/1 "{\"nodeId\":\"1\",\"address\":\"127.0.0.1:10000\",\"state\":\"BOOTING\"}"
-docker exec -it memdb-clusterdb etcdctl put /nodes/2 "{\"nodeId\":\"2\",\"address\":\"127.0.0.1:10001\",\"state\":\"BOOTING\"}"
+etcdctl put /nodes/1 "{\"nodeId\":\"1\",\"address\":\"127.0.0.1:10000\",\"state\":\"BOOTING\"}"
+etcdctl put /nodes/2 "{\"nodeId\":\"2\",\"address\":\"127.0.0.1:10001\",\"state\":\"BOOTING\"}"
 
 # CLUSTER MANAGER
 cd ./clustermanager
+
 ./build-and-run.sh &
 sleep 2s
 
@@ -27,6 +18,7 @@ cd ..
 
 # CLUSTER NODES
 cd ./core
+
 sudo ./linux-build.sh
 sudo ./run.sh MEMDB_CORE_USE_REPLICATION=true \
         MEMDB_CORE_SHOW_DEBUG_LOG=true \
@@ -34,6 +26,7 @@ sudo ./run.sh MEMDB_CORE_USE_REPLICATION=true \
         MEMDB_CORE_PERSISTANCE_WRITE_EVERY=1 \
         MEMDB_CORE_CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
         MEMDB_CORE_ETCD_ADDRESSES=127.0.0.1:2379 \
+        MEMDB_CORE_DATA_PATH=/etc/memdb1 \
         MEMDB_CORE_NODE_ID=1 &
 
 sleep 10s
@@ -44,4 +37,5 @@ sudo gdb --args ./src/build/src/memdb_run MEMDB_CORE_USE_REPLICATION=true \
         MEMDB_CORE_PERSISTANCE_WRITE_EVERY=1 \
         MEMDB_CORE_CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
         MEMDB_CORE_ETCD_ADDRESSES=127.0.0.1:2379 \
+        MEMDB_CORE_DATA_PATH=/etc/memdb2 \
         MEMDB_CORE_NODE_ID=1

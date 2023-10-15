19d87af22592d3d4797219d9b598f82b509c2455
Jaime Polidura <jaime.polidura@gmail.com>
2023-10-15 15:44:13 +0200

    Added multiple ways to start partitons test in _deploytment

---
+++ _deployment/partitions/manual/clear-oplogs.sh
@@ -0,0 +1,6 @@
+rm -r /etc/memdb1/0
+rm -r /etc/memdb2/0
+rm -r /etc/memdb3/0
+rm -r /etc/memdb4/0
+rm -r /etc/memdb5/0
+rm -r /etc/memdb6/0
--- _deployment/partitions/manual/debug/debug-partitions-1.sh
+++ _deployment/partitions/manual/debug/debug-partitions-1.sh
@@ -35,56 +35,11 @@ cd ..
 cd ./core
 sudo ./linux-build.sh
 
-sudo ./run.sh USE_REPLICATION=true \
+sudo gdb --args /src/build/src/memdb_run USE_REPLICATION=true \
         SHOW_DEBUG_LOG=true \
         SERVER_PORT=10000 \
         CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
         ETCD_ADDRESSES=127.0.0.1:2379 \
         DATA_PATH=/etc/memdb1 \
         NODE_ID=1 \ 
-		USE_PARTITIONS=true &
-
-sudo ./run.sh USE_REPLICATION=true \
-        SHOW_DEBUG_LOG=true \
-        SERVER_PORT=10001 \
-        CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
-        ETCD_ADDRESSES=127.0.0.1:2379 \
-        DATA_PATH=/etc/memdb2 \
-        NODE_ID=2 \ 
-		USE_PARTITIONS=true &
-
-sudo ./run.sh USE_REPLICATION=true \
-        SHOW_DEBUG_LOG=true \
-        SERVER_PORT=10002 \
-        CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
-        ETCD_ADDRESSES=127.0.0.1:2379 \
-        DATA_PATH=/etc/memdb3 \
-        NODE_ID=3 \ 
-		USE_PARTITIONS=true &
-
-sudo ./run.sh USE_REPLICATION=true \
-        SHOW_DEBUG_LOG=true \
-        SERVER_PORT=10003 \
-        CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
-        ETCD_ADDRESSES=127.0.0.1:2379 \
-        DATA_PATH=/etc/memdb4 \
-        NODE_ID=4 \ 
-		USE_PARTITIONS=true &
-
-sudo ./run.sh USE_REPLICATION=true \
-        SHOW_DEBUG_LOG=true \
-        SERVER_PORT=10004 \
-        CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
-        ETCD_ADDRESSES=127.0.0.1:2379 \
-        DATA_PATH=/etc/memdb5 \
-        NODE_ID=5 \ 
-		USE_PARTITIONS=true &
-
-sudo gdb --args /src/build/src/memdb_run USE_REPLICATION=true \
-        SHOW_DEBUG_LOG=true \
-        SERVER_PORT=10005 \
-        CLUSTER_MANAGER_ADDRESS=127.0.0.1:8080 \
-        ETCD_ADDRESSES=127.0.0.1:2379 \
-        DATA_PATH=/etc/memdb6 \
-        NODE_ID=6 \ 
-		USE_PARTITIONS=true &
+		USE_PARTITIONS=true
